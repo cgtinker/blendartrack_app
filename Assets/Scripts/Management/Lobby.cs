753873c86a5533d96bfffa4c1f56641849989ca0
dhsu <d.hsu@online.de>
2020-10-27 14:24:20 +0100

    iOS Pose Data is now exportable, fixed stale button references

--- Assets/Scripts/Management/Lobby.cs
+++ Assets/Scripts/Management/Lobby.cs
@@ -5,7 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Lobby : MonoBehaviour
 {
-    public static string ArCore_CameraPose = "CameraTracker";
+    public static string[] AndroidScenes =
+    {
+        "PoseDataTracker",
+        "FaceTracker",
+
+    };
+    public static string[] IOSScenes =
+    {
+        "PoseDataTracker",
+        "ShapeKeys"
+    };
+
+    public static string ArCore_CameraPose = "PoseDataTracker";
     public static string ArCore_FaceMesh = "FaceTracker";
     public static string ArKit_CameraPose = "CameraTracker";
     public static string ArKit_ShapeKeys = "Sample Scene";
