8ee9ed440e9d9928ffc210c64a5e0d5b79e1623f
dhsu <d.hsu@online.de>
2020-11-18 11:26:59 +0100

    Android Prototype 0.0.1
    First dev prototype including AR Pose Tracking and AR Face Tracking. Also implemented a json viewer and json file export.

---
+++ Assets/Scripts/Management/ArFunctionalityHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AssemblyCSharp.Assets.Scripts.Management
+{
+    public class ArFunctionalityHelper
+    {
+        public ArFunctionalityHelper()
+        {
+        }
+    }
+}
