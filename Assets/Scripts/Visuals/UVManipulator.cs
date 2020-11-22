58ceb457eb752541c916ffae2300279eda14254f
dhsu <d.hsu@online.de>
2020-11-22 23:26:15 +0100

    status update, android test build base

---
+++ Assets/Scripts/Visuals/UVManipulator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class UVManipulator : MonoBehaviour
+{
+    private void Start()
+    {
+        GetMeshUV();
+    }
+
+    void GetMeshUV()
+    {
+        Mesh mesh = this.gameObject.GetComponent<MeshFilter>().mesh;
+
+        Vector3[] vertices = GetVertices(mesh);
+        Vector2[] uvs = GetUV(vertices);
+
+        mesh.uv = uvs;
+    }
+
+    void shit()
+    {
+        //a triangle def
+        int[] triangle = new int[]
+        {
+            0, 2, 3,
+            3, 1, 0
+        };
+    }
+
+    Vector2[] GetUV(Vector3[] vertices)
+    {
+        Vector2[] uvs = new Vector2[vertices.Length];
+
+        Array.Sort(vertices);
+
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i] = new Vector2(x: vertices[i].x, y: vertices[i].z);
+        }
+
+        return uvs;
+    }
+
+    Vector3[] GetVertices(Mesh mesh)
+    {
+        Vector3[] vertices = mesh.vertices;
+
+        return vertices;
+    }
+}
---
+++ Assets/WireframeRenderer/Script/StaticWireframeRenderer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// ReSharper disable once UnusedMember.Global
+[RequireComponent( typeof( MeshFilter ) )]
+public class StaticWireframeRenderer: MonoBehaviour {
+    
+    private const float Distance = 1.0001f;
+    private static readonly Color Color = Color.black;
+    private List<Vector3> _renderingQueue;
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public Material WireMaterial;
+
+    private void InitializeOnDemand() {
+        if (_renderingQueue != null) {
+            return;
+        }
+        var meshFilter = gameObject.GetComponent<MeshFilter>();
+        if ( meshFilter == null ) {
+            Debug.LogError( "No mesh detected at" + gameObject.name, gameObject );
+            return;
+        }
+        var mesh = meshFilter.mesh;
+
+        _renderingQueue = new List<Vector3>();
+        foreach (var point in mesh.triangles) {
+            _renderingQueue.Add( mesh.vertices[point] * Distance );
+        }
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public void OnPreRender() {
+        GL.wireframe = true;
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public void OnRenderObject() {
+        InitializeOnDemand();
+
+        if (WireMaterial != null) {
+            WireMaterial.SetPass(0);
+        } else {
+            var color = Color;
+            GL.Color( color );
+        }
+
+        GL.MultMatrix( transform.localToWorldMatrix );
+        GL.Begin( GL.LINES );
+
+        for ( var i = 0; i < _renderingQueue.Count; i+=3 ) {
+            var vertex1 = _renderingQueue[i];
+            var vertex2 = _renderingQueue[i+1];
+            var vertex3 = _renderingQueue[i+2];
+            GL.Vertex3( vertex1.x, vertex1.y, vertex1.z );
+            GL.Vertex3( vertex2.x, vertex2.y, vertex2.z );
+            GL.Vertex3( vertex2.x, vertex2.y, vertex2.z );
+            GL.Vertex3( vertex3.x, vertex3.y, vertex3.z );
+            GL.Vertex3( vertex3.x, vertex3.y, vertex3.z );
+            GL.Vertex3( vertex1.x, vertex1.y, vertex1.z );
+        }
+        GL.End();
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public void OnPostRender() {
+        GL.wireframe = false;
+    }
+
+}
