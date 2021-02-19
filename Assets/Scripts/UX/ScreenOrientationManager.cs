e1388f3f86011f560958dc54b09df2d443c302bd
dhsu <d.hsu@online.de>
2021-02-19 23:45:36 +0100

    UI State Machine

---
+++ Assets/Scripts/UX/ScreenOrientationManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ScreenOrientationManager
+{
+    public static void SetOrientation(bool portait)
+    {
+        try
+        {
+            SetAutoRotation(portait);
+
+            if (!portait)
+                Screen.orientation = ScreenOrientation.AutoRotation;
+
+            else
+                Screen.orientation = ScreenOrientation.Portrait;
+        }
+
+        catch
+        {
+            Debug.LogError("Cannot set correct screen orientation");
+        }
+    }
+
+    private static void SetAutoRotation(bool cur)
+    {
+        Screen.autorotateToLandscapeLeft = cur;
+        Screen.autorotateToLandscapeRight = cur;
+    }
+}
---
+++ Assets/Scripts/UX/UIStateMachine.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ArRetarget
+{
+    public class UIStateMachine : Singleton<UIStateMachine>
+    {
+        public enum State
+        {
+            StartUp,
+            Update,
+            Tutorial,
+            Tracking,
+            Filebrowser,
+            Settings
+        }
+
+        private State uiState;
+
+        public void SetState(State state)
+        {
+            uiState = state;
+            UpdateState();
+        }
+
+        private void UpdateState()
+        {
+            switch (uiState)
+            {
+                case State.StartUp:
+                    break;
+
+                case State.Update:
+                    break;
+
+                case State.Tutorial:
+                    break;
+
+                case State.Tracking:
+                    break;
+
+                case State.Filebrowser:
+                    break;
+
+                case State.Settings:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
+
