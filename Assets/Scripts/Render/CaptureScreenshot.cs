0ae1c9a4cdd0c3c1c9ab26948c93a459920af9fd
dhsu <d.hsu@online.de>
2021-04-16 14:28:22 +0200

    screenshot util

---
+++ Assets/Scripts/Render/CaptureScreenshot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureScreenshot : MonoBehaviour
+{
+	private static int imgCount = 0;
+	private string path;
+
+	public int sizeFactor = 1;
+	public string imgTitel = "BlendArTrack";
+	public KeyCode keyCode;
+
+	private void Start()
+	{
+		imgCount = 0;
+		path = Application.persistentDataPath;
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(keyCode))
+		{
+			OnCaptureScreenshot();
+		}
+	}
+
+	public void OnCaptureScreenshot()
+	{
+		imgCount++;
+		string filename = $"{path}/{imgTitel}_{imgCount}.png";
+
+		ScreenCapture.CaptureScreenshot(filename, sizeFactor);
+		Debug.Log($"saved {imgTitel}_{imgCount} at persistent path");
+	}
+}
